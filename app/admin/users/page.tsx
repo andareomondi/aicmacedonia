42b3c44069a942de8d8a51f4cce55eb89c97f6e0
shadrackandare <andareomondi@gmail.com>
2025-07-26 07:37:36 +0300

    using anon key not what ever was there

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -15,7 +15,7 @@ export default async function UsersPage({
   const cookieStore = cookies()
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get: (name) => cookieStore.get(name)?.value,
